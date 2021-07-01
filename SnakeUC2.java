8f432bf69091aaffef53f8802ff582b381639f3a
ishuks9 <ishuks9@gmail.com>
2021-07-01 18:19:07 +0530

    use case2

---
+++ SnakeUC2.java
@@ -0,0 +1,8 @@
+public class SnakeUC2{
+	public static void main(String args[]){
+		System.out.println("Welcome to the Game");
+		int pos = 0;
+		int s = (int) Math.floor(Math.random()*10)%6;
+		System.out.println(s);
+	}
+}
